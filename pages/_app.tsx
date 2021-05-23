f5d6fc716176de90c6ac20bfb1194977c90298a9
ossa03 <kfcxd953pelo@gmail.com>
2021-05-23 22:43:39 +0900

    setup typescript & tailwindcss

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />
+}
+
+export default App
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { FC } from 'react'
+
+const Home: FC = () => {
+	return (
+		<div>
+			<Head>
+				<title>Hulu clone</title>
+			</Head>
+		</div>
+	)
+}
+
+export default Home
